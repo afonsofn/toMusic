451ed170df1c4f6c8866ada30774ff1d6e0f99bd
afonso <afonsoafn@gmail.com>
2021-06-28 12:13:51 -0300

    subindo novo slider

---
+++ src/components/Slider.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="carousel">
+    <a class="carousel-item" href="#one!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 1</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+    <a class="carousel-item" href="#two!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 2</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+    <a class="carousel-item" href="#three!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 3</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+    <a class="carousel-item" href="#four!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 4</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+    <a class="carousel-item" href="#five!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 5</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+    <a class="carousel-item" href="#six!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 6</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+    <a class="carousel-item" href="#seven!">
+      <div class="teste">
+        <p class="cover"></p>
+        <div class="playlist-info">
+          <p class="playlist-name">PlayList 7</p> 
+          <p class="playlist-description">description...</p>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<style src="@/styles/slider.scss" lang="scss" scoped />
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,48 +1,7 @@
 <template>
   <div class="home-container">
-    <div class="main-content">
-      <!-- 4 playlists se tiverem, (ao lado da ultima playlist se tiver mais plylists ? ver todas : um button para criar uma nova )
-      se nao tiver playlist sugestoes de 4 artistas e um botao para criar playlists -->
-      <div class="playlist-container">
-        <p class="cover"></p>
-        <div class="playlist-info">
-          <p class="playlist-name">PlayList 1</p> 
-          <p class="playlist-description">description...</p>
-        </div>
-      </div>
-     
-      <div class="playlist-container">
-        <p class="cover"></p>
-        <div class="playlist-info">
-          <p class="playlist-name">PlayList 2</p> 
-          <p class="playlist-description">description...</p>
-        </div>
-      </div>
+    <Slider />
 
-      <div class="playlist-container">
-        <p class="cover"></p>
-        <div class="playlist-info">
-          <p class="playlist-name">PlayList 3</p> 
-          <p class="playlist-description">description...</p>
-        </div>
-      </div>
-
-      <div class="playlist-container">
-        <p class="cover"></p>
-        <div class="playlist-info">
-          <p class="playlist-name">PlayList 4</p> 
-          <p class="playlist-description">description...</p>
-        </div>
-      </div>
-
-      <div class="playlist-container">
-        <p class="cover"></p>
-        <div class="playlist-info">
-          <p class="playlist-name">PlayList 5</p> 
-          <p class="playlist-description">description...</p>
-        </div>
-      </div>
-    </div>
     <div class="categorys">
       playlists separadas por categoria
     </div>
@@ -50,10 +9,12 @@
 </template>
 
 <script>
+import Slider from '../components/Slider.vue'
 
 export default {
   name: 'Home',
   components: {
+    Slider
   }
 }
 </script>
