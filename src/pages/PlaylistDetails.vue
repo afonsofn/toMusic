9422c22f717eb444a624ea81abd3685e0e71084d
afonso <afonsoafn@gmail.com>
2021-08-10 11:37:39 -0300

    terminando estilo da table

--- src/pages/PlaylistDetails.vue
+++ src/pages/PlaylistDetails.vue
@@ -24,7 +24,8 @@
       <div class="nav-playlist">
         <div class="left-nav">
           <img src="../assets/play.svg" alt="" class="play-btn">
-          <i class="material-icons">favorite_border</i>
+          <i v-if="favorite === true" class="material-icons empty" @click="makeFavorite()">favorite_border</i>
+          <i v-else class="material-icons fullfil" @click="makeFavorite()">favorite</i>
         </div>
 
         <router-link to="/" >
@@ -34,87 +35,26 @@
       
       <table>
         <thead>
-          <tr>
-            <th>#</th>
-            <th>TITLE</th>
-            <th>ALBUM</th>
-            <th>DATE ADDED</th>
-            <th class="last">DURATION</th>
-          </tr>
+          <th>#</th>
+          <th>TITLE</th>
+          <th>ARTIST</th>
+          <th>DATE ADDED</th>
+          <th class="last">DURATION</th>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-            <td class="last">$0.87</td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-            <td class="last">$3.76</td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-            <td class="last">$7.00</td>
-          </tr>
-
-          <tr>
-            <td>1</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-            <td class="last">$0.87</td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-            <td class="last">$3.76</td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-            <td class="last">$7.00</td>
-          </tr>
-
-          <tr>
-            <td>1</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-            <td class="last">$0.87</td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-            <td class="last">$3.76</td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Alvin Alan Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-            <td class="last">$7.00</td>
-          </tr>
+          <template v-for="(track, index) in playlistDetails.tracks" v-bind:key="index">
+            <tr>
+              <td class="first">
+                <span>{{ index + 1 }}</span>
+                <img src="../assets/play.svg" alt="" class="play-btn">
+              </td>
+              <td>{{ track.title }}</td>
+              <td>{{ track.artist }}</td>
+              <td>{{ track.date }}</td>
+              <td class="last">{{ track.duration }}</td>
+            </tr>
+          </template>
         </tbody>
       </table>
     </section>
@@ -133,7 +73,13 @@
     data () {
       return {
         playlistDetails: computed(() => this.$store.state.playListOnDetail),
-        loading: computed(() => this.$store.state.loadingDetails)
+        loading: computed(() => this.$store.state.loadingDetails),
+        favorite: true
+      }
+    },
+    methods: {
+      makeFavorite() {
+        this.favorite = !this.favorite
       }
     },
     created() {
